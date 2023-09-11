4b91d98f4ce1783358be61bda9473845d291733c
SodaXI <jangbk1101@gmail.com>
2023-09-11 14:55:51 +0900

    FIX : Materials Converter
    When a map is generated, the tiles around objects change to match their background.

--- Project_SWENG/Assets/Scripts/Level_Tile/TileField/MaterialsConverter.cs
+++ Project_SWENG/Assets/Scripts/Level_Tile/TileField/MaterialsConverter.cs
@@ -8,14 +8,15 @@ public class MaterialsConverter : MonoBehaviour
 
     public void ConvertMat(List<GameObject> objTiles)
     {
+        // CHK
+        Debug.Log("Chg MAT");
         foreach (GameObject objTile in objTiles)
         {
             Transform selectFolder = objTile.transform.Find("Main");
             Renderer renderer = selectFolder.GetComponentInChildren<Renderer>();
-            if (renderer != null) continue;
+            if (renderer == null) continue;
 
             Material newMat = renderer.material;
-
             Hex hex = objTile.GetComponent<Hex>();
 
             foreach (var aroundTile in HexGrid.Instance.GetNeighboursDoubleFor(hex.HexCoords))
@@ -24,7 +25,8 @@ public class MaterialsConverter : MonoBehaviour
 
                 if (aroundTileHex != null)
                 {
-                    if (aroundTileHex.tileType == Hex.Type.Object || aroundTileHex.tileType == Hex.Type.Obstacle) continue;
+
+                    if (aroundTileHex.tileType == Hex.Type.Object || aroundTileHex.tileType == Hex.Type.Obstacle || aroundTileHex.tileType == Hex.Type.Water) continue;
                     Transform selectFolderInAroundTile = aroundTileHex.gameObject.transform.Find("Main");
                     Renderer rendererInAroundTile = selectFolderInAroundTile.GetComponentInChildren<Renderer>();
                     if (rendererInAroundTile != null)
