88c6303c08816106ec7129f9fb0e7c336195aa52
SodaXI <jangbk1101@gmail.com>
2023-09-13 23:54:44 +0900

    Health animation

--- Project_SWENG/Assets/Scripts/Player/GUI_PlayerHealth.cs
+++ Project_SWENG/Assets/Scripts/Player/GUI_PlayerHealth.cs
@@ -13,10 +13,11 @@ public class GUI_PlayerHealth : MonoBehaviour
     [SerializeField] Image healthBack;
     [SerializeField] Image afterimage;
     [SerializeField] TextMeshProUGUI hpText;
+    
+    public float lerpSpeed = 0.5f;
+    public float updateInterval = 0.1f;
 
-    int curHealth;
-
-    float duration = 0.1f;
+    float curValue;
 
     private void Awake()
     {
@@ -28,16 +29,25 @@ public class GUI_PlayerHealth : MonoBehaviour
         health = player.GetComponent<PlayerHealth>();
         SetHealth(health.maxHealth);
         afterimage.fillAmount = 1;
+        curValue = health.maxHealth;
     }
 
     void GUI_Damaged(object sender, IntEventArgs e)
     {
         SetHealth(e.Value);
+        StartCoroutine(LerpValue(e.Value));
+    }
 
-        while (Mathf.Approximately(curHealth, e.Value))
+    private IEnumerator LerpValue(float endValue)
+    {
+        float elapsedTime = 0f;
+        float startValue = curValue;
+        while (elapsedTime < 1f)
         {
-            float decrease = Mathf.Lerp(curHealth, e.Value, duration);
-            afterimage.fillAmount = (float)(decrease / health.maxHealth);
+            curValue = Mathf.Lerp(startValue, endValue, elapsedTime);
+            afterimage.fillAmount = (float)(curValue / health.maxHealth);
+            elapsedTime += Time.deltaTime * lerpSpeed;
+            yield return new WaitForSeconds(updateInterval);
         }
     }
 
