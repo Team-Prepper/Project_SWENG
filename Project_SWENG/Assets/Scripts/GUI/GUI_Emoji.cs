463d94f1ffb3be758cd5a1f4fb5c3279a33d9729
EasyH <skysea001010@gmail.com>
2023-11-26 21:33:52 +0900

    이모지 1차 구현 외
    이모지 1차 구현
    	이모지 버튼을 누르면 플레이어 정보 왼쪽에 이모지가 띄워짐
    플레이어 준비가 덜 되었을 때 시작 버튼을 누르면 플레이어가 부족하다는 정보 알림
    	준비되지 않은 플레이어가 있습니다 번역 필요(한국어는 추가됨)
    	1인 플레이시(출력 문구 필요 없음) 문제 발생하지 않음
    	2인 이상 플레이시 작동 확인 필요
    네트워크 로비에 들어갈 때 정보 초기화되도록 수정

---
+++ Project_SWENG/Assets/Scripts/GUI/GUI_Emoji.cs
@@ -0,0 +1,41 @@
+﻿using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+using UnityEngine.Windows;
+
+public class GUI_Emoji : MonoBehaviour {
+
+    NetworkManager _network;
+
+    [SerializeField] GUI_Network_Room_PlayerState[] _players;
+    [SerializeField] SpriteAtlas _atlas;
+
+    private void Start()
+    {
+        _network = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+
+        _network._emoji = this;
+    }
+
+    public void EmojiSend(string emojiKey)
+    {
+        _network.SendEmoji(emojiKey);
+
+    }
+
+    public void RecieveEmoji(string sender, string emojiKey)
+    {
+        Player[] memberList = _network.RoomMemberList();
+
+        for (int i = 0; i < _players.Length; i++) {
+            if (memberList[i].Equals(sender)) continue;
+            _players[i].DisplayEmoji(_atlas.GetSprite(emojiKey));
+            return;
+        }
+    }
+
+}
