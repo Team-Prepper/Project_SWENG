307080de0157ec638f4b495bb557ef1ebaa30f57
SodaXI <jangbk1101@gmail.com>
2023-12-01 23:48:35 +0900

    EffectFix

--- Project_SWENG/Assets/Scripts/Effect/EffectManager.cs
+++ Project_SWENG/Assets/Scripts/Effect/EffectManager.cs
@@ -5,17 +5,11 @@ using UnityEngine;
 
 public class EffectManager : MonoSingleton<EffectManager>
 {
-    [Header("Network")]
-    private PhotonView _photonView;
 
     [SerializeField] private List<GameObject> ImpactVfx;
     private float impactVfxLifetime = 3f;
     private GameObject target;
 
-    private void Start()
-    {
-        _photonView = GetComponent<PhotonView>();
-    }
     public void SetTarget(GameObject target)
     {
         this.target = target;
@@ -23,13 +17,6 @@ public class EffectManager : MonoSingleton<EffectManager>
     
     public void ShowImpactVfxHandler(int impactCode)
     {
-        _photonView.RPC("ShowImpactVfxCoHandler", RpcTarget.All, impactCode);
-    }
-
-    [PunRPC]
-    private void ShowImpactVfxCoHandler(int impactCode)
-    {
-        Debug.Log("ShowEffect");
         StartCoroutine(ShowImpactVfx(impactCode));
     }
 
