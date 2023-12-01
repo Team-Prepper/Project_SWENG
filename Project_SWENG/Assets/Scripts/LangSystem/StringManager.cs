a054407f9a642957bfbee823d1bd6866f4ae2a5a
SodaXI <jangbk1101@gmail.com>
2023-12-01 16:56:53 +0900

    BACKUP
    CHANGEFONT

--- Project_SWENG/Assets/Scripts/LangSystem/StringManager.cs
+++ Project_SWENG/Assets/Scripts/LangSystem/StringManager.cs
@@ -44,7 +44,7 @@ namespace LangSystem {
 
         Dictionary<string, string> _dic;
 
-        string _nowLang = "Eng";
+        string _nowLang = "Kor";
 
         protected override void OnCreate()
         {
